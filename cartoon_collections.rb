5fd6069fc5004d55d89f4e7c2cad5850632a5dcd
Roberto Medina <robby.medina562@gmail.com>
2019-08-24 06:04:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,5 @@ def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   while i < cheese_types.length
   return cheese_types[i]
-  array.include
+  array.include?(cheese_types[i])
 end
