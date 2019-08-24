748a06867b2a1fad248f0f896a12978d5c40338c
Roberto Medina <robby.medina562@gmail.com>
2019-08-24 06:00:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese()
-  # the array below is here to help
+def find_the_cheese(strings)
+  strings.inclued? do 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
