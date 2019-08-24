8227ad926a5180328cc6e5422959dd23bf5dab1a
Roberto Medina <robby.medina562@gmail.com>
2019-08-24 05:57:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,9 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.each |
+  calls.any? do |word|
+    word > 4
+  end
 end
 
 def find_the_cheese# code an argument here
