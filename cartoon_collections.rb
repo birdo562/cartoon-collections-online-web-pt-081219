1d88f098da1aaca11d620540cf73f1e66aab942e
Roberto Medina <robby.medina562@gmail.com>
2019-08-24 05:52:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(names)
 end
 end
 
-def summon_captain_planet
+def summon_captain_planet(calls)
+  calls.map do |elements| 
 end
 
 def long_planeteer_calls# code an argument here
